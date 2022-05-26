13199b0d90e942820d9882d552e8abe3e5b3ca79
Marisol Rodriguez <maridrodriguez@gmail.com>
2022-05-25 21:28:58 -0400

    initial commit: completed activity for the basic criteria.

---
+++ models/language.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const languageSchema = new Schema ({
+    name: { type: String, required: true },
+    greeting: String,
+    pangram: String, 
+    filler: String 
+})
+
+// model and export
+const Language = mongoose.model('Language', languageSchema)
+module.exports = Language
